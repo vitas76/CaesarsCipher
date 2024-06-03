2480ca5593c9754eed00c22d34fb13432e793a9c
vitas76 <hosek76@gmail.com>
2024-06-03 21:41:21 +0200

    Pridano readme.md a doplněn comment CesarSifraModul2.py

--- CesarSifraModul2.py
+++ CesarSifraModul2.py
@@ -1,4 +1,8 @@
 # *** Cesarova šifra jako modul ***
+# Source of graphic logo: http://www.patorjk.com/software/taag/#p=testall&f=Graffiti&t=CaesarsCipher
+# Source of code inspiration : https://youtu.be/BWtRPpGj-vw?list=PLQ8x_VWW6AktSriRsoH7LH_-t0LqS6aa_
+# And thanks for the support Martin T.
+
 alphabet = [' ', '.', ',', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 ## Caesaser Cipher
+** Source of graphic logo:  
+http://www.patorjk.com/software/taag/#p=testall&f=Graffiti&t=CaesarsCipher **
+** Source of code inspiration:  
+https://youtu.be/BWtRPpGj-vw?list=PLQ8x_VWW6AktSriRsoH7LH_-t0LqS6aa_ **
+** And thanks for the support Martin T. **  
 > EXAMPL: slovo = "yzabc" / "vwxyz" ; kodovani = 3; enc_dec = True/False (Encoding/Decoding)
 
